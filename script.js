533af62d27bc5026c7d6bf27f2118d352e6977b7
ArturBystranowski <artur.bystranowski@gmail.com>
2022-09-09 14:49:53 +0200

    add RWD, update map loading

--- script.js
+++ script.js
@@ -8,6 +8,22 @@ const locationValue = document.querySelector(".location-value");
 const timezoneValue = document.querySelector(".timezone-value");
 const ispValue = document.querySelector(".isp-value");
 
+// async function getData() {
+// 	let ip = ipInput.value;
+// 	const response = await fetch(API_URL + ip);
+// 	const data = await response.json();
+// 	ipValue.textContent = data.ip;
+// 	locationValue.textContent = `${data.location.country}, ${data.location.city}`;
+// 	timezoneValue.textContent = `UTC${data.location.timezone}`;
+// 	ispValue.textContent = data.isp;
+// 	let lat = data.location.lat;
+// 	let lng = data.location.lng;
+// }
+
+// ---------------------------------------------
+
+let map;
+
 async function getData() {
 	let ip = ipInput.value;
 	const response = await fetch(API_URL + ip);
@@ -19,12 +35,29 @@ async function getData() {
 	let lat = data.location.lat;
 	let lng = data.location.lng;
 
-	let map = L.map("map").setView([lat, lng], 16);
+	getMap(lat, lng);
+}
+
+const getMap = (lat, lng) => {
+	map.setView([lat, lng], 14);
+
+	let icon = L.icon({
+		iconUrl: "images/icon-location.svg",
+		iconSize: [40, 48],
+	});
+
+	L.marker([lat, lng], { icon }).addTo(map);
+};
+
+(function () {
+	map = L.map("map", { zoomControl: false, scrollWheelZoom: false });
 
 	L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
 		maxZoom: 19,
 		attribution: "© OpenStreetMap",
 	}).addTo(map);
-}
-getData();
+
+	getData();
+})();
+
 btn.addEventListener("click", getData);
